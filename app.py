d4ad1a536943eca74a0dd223b4ae80d826ba5b2e
AdenC123 <adenrudy23@gmail.com>
2022-08-13 17:54:02 -0700

    Site is running open, close, and send_time methods

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,5 +8,26 @@ def index():
     return render_template('Home.html')
 
 
+@app.route('/open')
+def opens():
+    print('opening...')
+    return render_template('Home.html')
+
+
+@app.route('/close')
+def closes():
+    print('closing...')
+    return render_template('Home.html')
+
+
+@app.route('/send_times', methods=['POST'])
+def update_times():
+    open_time = request.form['open-time']
+    close_time = request.form['close-time']
+    print(open_time)
+    print(close_time)
+    return render_template('Home.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5100)
