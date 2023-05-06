07192f5f2a0879c3701e0f99985abdc013a7335a
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2023-05-05 20:02:32 -0700

    manual api untested

---
+++ door_api.py
@@ -0,0 +1,44 @@
+# Start the API for direct door control.
+from flask import Flask
+from gpiozero import Motor
+import threading
+
+
+# constants
+API_PORT = 5200
+
+MOTOR_FORWARD = 23
+MOTOR_BACKWARD = 24
+MOTOR_ENABLE = 25
+MOTOR_DELAY = 5  # time to run in seconds
+
+# flask initialization
+app = Flask(__name__)
+
+# motor setup with ports
+motor = Motor(MOTOR_FORWARD, MOTOR_BACKWARD, MOTOR_ENABLE)
+
+
+# Stops the motor after a delay
+def stop_after(delay: int):
+    threading.Timer(delay, motor.stop).start()
+
+
+# Open the door manually (if it isn't currently moving)
+@app.route('/open', methods='POST')
+def open_door():
+    if not motor.is_active:
+        motor.forward()
+        stop_after(MOTOR_DELAY)
+
+
+# Close the door manually (if it isn't currently moving)
+@app.route('/close', methods='POST')
+def close_door():
+    if not motor.is_active:
+        motor.backward()
+        stop_after(MOTOR_DELAY)
+
+
+# start the api
+app.run(port=API_PORT, host="0.0.0.0")
