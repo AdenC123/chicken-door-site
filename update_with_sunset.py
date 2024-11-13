4ff49b88c1ab3b6704f7919145503161a6daa20a
AdenC123 <39308648+AdenC123@users.noreply.github.com>
2024-11-12 19:54:06 -0800

    use old format

--- update_with_sunset.py
+++ update_with_sunset.py
@@ -13,7 +13,7 @@ tz = pytz.timezone('US/Pacific')
 # get open and close times based on sunrise and (sunset + 30 mins)
 open_time = sun.get_sunrise_time(time_zone=tz).strftime('%H:%M')
 close_time = (sun.get_sunset_time(time_zone=tz) + datetime.timedelta(minutes=30)).strftime('%H:%M')
-print(f"open: {open_time} close: {close_time}")
+print("open: {} close: {}".format(open_time, close_time))
 
 # schedule open and close scripts at new times
 cron.set_times(open_time, close_time)
